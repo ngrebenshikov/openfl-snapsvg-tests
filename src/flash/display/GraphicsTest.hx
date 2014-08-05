3eedb635d060dc0d573d62884d0d4f94d624d19a
Nickolay Grebenshikov <grebenshikov.n@gmail.com>
2014-08-05 16:24:02 +0800

    Graphics.lineTo test finished.

--- src/flash/display/GraphicsTest.hx
+++ src/flash/display/GraphicsTest.hx
@@ -5,20 +5,66 @@ import massive.munit.async.AsyncFactory;
 import flash.events.Event;
 
 class GraphicsTest {
-    @Test
-    public function testLineTo() {
-        var g:Graphics = Lib.current.graphics;
-        g.lineStyle();
-        g.beginFill(0xff0000, 1);
-        g.drawRoundRect(50, 50, 500, 500, 20);
-        g.endFill();
+    private var g: Graphics;
 
-        g.lineStyle(10, 0);
+    @BeforeClass
+    public function beforeClass():Void {}
+
+    @AfterClass
+    public function afterClass():Void {}
+
+    @Before
+    public function setup():Void {
+        g = Lib.current.graphics;
+        g.clear();
+    }
+
+    @After
+    public function tearDown():Void {
+        g.clear();
+    }
+
+    @AsyncTest
+    public function testLineTo1(asyncFactory: AsyncFactory) {
+        var g:Graphics = Lib.current.graphics;
+        g.lineStyle(10, 0x123456, 0.4, true, LineScaleMode.NORMAL, CapsStyle.ROUND, JointStyle.BEVEL, 5);
+        g.beginFill(0x654321, 0.4);
         g.moveTo(100,100);
         g.lineTo(200, 200);
-        g.lineTo(200, 300);
 
-        //g.clear();
+        testLineToHandler = asyncFactory.createHandler(this, function() {
+            var path = g.__snap.select("path");
+            Assert.isNotNull(path);
+            Assert.areEqual("M100 100 L200 200 Z", path.attr("d"));
+            Assert.areEqual("rgba(18, 52, 86, 0.4)", path.attr("stroke"));
+            Assert.areEqual("rgba(101, 67, 33, 0.4)", path.attr("fill"));
+            Assert.areEqual("stroke-width: 10px; stroke-linecap: round; stroke-linejoin: bevel; stroke-miterlimit: 5;", path.attr("style"));
+            Assert.areEqual("none", path.attr("vector-effect"));
+            testLineToHandler = null;
+        }, 300);
+        Lib.__getStage().addEventListener(Event.STAGE_RENDERED, testLineToHandler);
+    }
+    private var testLineToHandler: Dynamic;
+
+    @AsyncTest
+    public function testLineTo2(asyncFactory: AsyncFactory) {
+        var g:Graphics = Lib.current.graphics;
+        g.lineStyle(10, 0x123456, 0.4, true, LineScaleMode.NONE, CapsStyle.SQUARE, JointStyle.ROUND, 5);
+        g.beginFill(0x654321, 0.4);
+        g.moveTo(23, 567);
+        g.lineTo(987, 456);
+
+        testLineToHandler = asyncFactory.createHandler(this, function() {
+            var path = g.__snap.select("path");
+            Assert.isNotNull(path);
+            Assert.areEqual("M23 567 L987 456 Z", path.attr("d"));
+            Assert.areEqual("rgba(18, 52, 86, 0.4)", path.attr("stroke"));
+            Assert.areEqual("rgba(101, 67, 33, 0.4)", path.attr("fill"));
+            Assert.areEqual("stroke-width: 10px; stroke-linecap: square; stroke-linejoin: round; stroke-miterlimit: 5;", path.attr("style"));
+            Assert.areEqual("non-scaling-stroke", path.attr("vector-effect"));
+            testLineToHandler = null;
+        }, 300);
+        Lib.__getStage().addEventListener(Event.STAGE_RENDERED, testLineToHandler);
     }
 
     @AsyncTest
