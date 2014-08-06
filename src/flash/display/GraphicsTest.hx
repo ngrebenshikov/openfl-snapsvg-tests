82d6502894ff7cf31e7dd97a45b70c29a65f77ed
amanzhugin <tupieurods@gmail.com>
2014-08-06 22:14:58 +0800

    Asserts added to Graphics.curveTo test

--- src/flash/display/GraphicsTest.hx
+++ src/flash/display/GraphicsTest.hx
@@ -71,21 +71,41 @@ class GraphicsTest {
     }
 
     @AsyncTest
-    public function testCurveTo(asyncFactory: AsyncFactory) {
-        g.lineStyle(5, 0x0000FF);
+    public function testCurveTo1(asyncFactory: AsyncFactory) {
+        g.lineStyle(5, 0x0000FF, 0.7, true, LineScaleMode.NONE, CapsStyle.ROUND, JointStyle.ROUND, 7);
         g.moveTo(250, 200);
         g.curveTo(300, 0, 350, 200);
-        g.curveTo(400, 450 , 500, 200);
-        testCurveHandler = asyncFactory.createHandler(this, assertsTestCurveTo, 300);
+        testCurveHandler = asyncFactory.createHandler(this, function() {
+            var path = g.__snap.select("path");
+            Assert.isNotNull(path);
+            Assert.areEqual("M250 200 Q300 0 350 200 Z", path.attr("d"));
+            Assert.areEqual("rgba(0, 0, 255, 0.7)", path.attr("stroke"));
+            Assert.areEqual("transparent", path.attr("fill"));
+            Assert.areEqual("stroke-width: 5; stroke-linecap: round; stroke-linejoin: round; stroke-miterlimit: 7;", path.attr("style"));
+            Assert.areEqual("non-scaling-stroke", path.attr("vector-effect"));
+            testCurveHandler = null;
+        }, 300);
         Lib.__getStage().addEventListener(Event.STAGE_RENDERED, testCurveHandler);
     }
 
     private var testCurveHandler: Dynamic;
 
-    private function assertsTestCurveTo() {
-        Lib.__getStage().removeEventListener(Event.STAGE_RENDERED, testCurveHandler);
-        testCurveHandler = null;
-        trace(g.__snap);
-        Assert.isTrue(true);
+    @AsyncTest
+    public function testCurveTo2(asyncFactory: AsyncFactory) {
+        g.lineStyle(30, 0xFFEEAA, 0.3, true, LineScaleMode.NONE, CapsStyle.SQUARE, JointStyle.BEVEL, 4);
+        g.beginFill(0x642A15, 0.85);
+        g.moveTo(200, 250);
+        g.curveTo(400, 450 , 500, 200);
+        testCurveHandler = asyncFactory.createHandler(this, function() {
+            var path = g.__snap.select("path");
+            Assert.isNotNull(path);
+            Assert.areEqual("M200 250 Q400 450 500 200 Z", path.attr("d"));
+            Assert.areEqual("rgba(255, 238, 170, 0.3)", path.attr("stroke"));
+            Assert.areEqual("rgba(100, 42, 21, 0.85)", path.attr("fill"));
+            Assert.areEqual("stroke-width: 30; stroke-linecap: square; stroke-linejoin: bevel; stroke-miterlimit: 4;", path.attr("style"));
+            Assert.areEqual("non-scaling-stroke", path.attr("vector-effect"));
+            testCurveHandler = null;
+        }, 300);
+        Lib.__getStage().addEventListener(Event.STAGE_RENDERED, testCurveHandler);
     }
 }
